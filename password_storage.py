cbd01ea6d08ac5483d6f2946e388dc3f9bf86ff0
Shldap <142171908+Shldap@users.noreply.github.com>
2023-12-04 17:32:13 +0000

    Update password_storage.py

--- password_storage.py
+++ password_storage.py
@@ -1,1 +1,14 @@
+class PasswordStorage:
+    def __init__(self):
+        self.passwords = {}
 
+    def add_password(self, website, username, password):
+        if website in self.passwords:
+            self.passwords[website][username] = password
+        else:
+            self.passwords[website] = {username: password}
+
+    def get_password(self, website, username):
+        if website in self.passwords and username in self.passwords[website]:
+            return self.passwords[website][username]
+        return None
